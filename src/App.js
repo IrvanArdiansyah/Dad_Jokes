de999840f145cc8dfe536b592d49470ddc0fe9cf
IrvanArdiansyah <irvardiansyah@gmail.com>
2018-12-11 15:17:54 +0700

    ini soal 2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import './Style.css'
+import axios from 'axios'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      setup: '',
+      punchline: ''
+    }
+  }
+
   render() {
+    let klik = () => {
+      let url = `https://safe-falls-22549.herokuapp.com/random_joke`
+      axios.get(url)
+        .then((info) => {
+          this.setState({
+            setup: info.data.setup,
+            punchline: info.data.punchline
+          })
+        })
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <center>
+      <div class="card mt-5" style={{width: '30rem', height: '12rem'}}>
+        <div class="card-body">
+          <h5 class="card-title">{this.state.setup}</h5>
+          <p class="card-text">{this.state.punchline}</p>
+          <button onClick={klik} href="#" class="btn btn-danger reload">Reload 💫</button>
+        </div>
       </div>
-    );
+      </center>
+
+    )
   }
 }
 
